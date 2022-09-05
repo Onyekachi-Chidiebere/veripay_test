ac24131a717fed6d05f88a0f7159d010a01806a4
Onyekachi-Chidiebere <onyekachisamuel18@gmai.com>
2022-09-05 15:36:12 +0100

    added port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const app = express();
+const port = process.env.PORT||5001
 
 app.get('*',(req, res)=>{
     console.log('route hit')
@@ -9,5 +10,5 @@ app.get('*',(req, res)=>{
 })
 app.listen(5001, () => {
     //listen to port
-    console.log(`Server started at http://localhost:5001`);
+    console.log(`Server started at http://localhost:${port}`);
   });
