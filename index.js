0853005792edc44a6f01caa553d9f19454cf0f89
Onyekachi-Chidiebere <onyekachisamuel18@gmai.com>
2022-09-05 15:40:43 +0100

    new port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT||5001
+const port = process.env.PORT||7021
 
 app.get('*',(req, res)=>{
     console.log('route hit')
